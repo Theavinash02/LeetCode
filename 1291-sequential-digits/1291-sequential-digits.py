48209def176015d1675b1bd44a57e2b54a450256
Avinash <103890061+Theavinash02@users.noreply.github.com>
2026-07-13 23:12:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.24%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,14 +1,17 @@
+from collections import deque
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        sample = "123456789"
-        ans =[]
+        queue = deque(range(1,10))
+        res = []
 
-        lows = len(str(low))
-        highs = len(str(high))
-
-        for length in range(lows,highs+1):
-            for start in range(9+1-length):
-                arr = int(sample[start:start+length])
-                if low <= arr <= high:
-                    ans.append(arr)
-        return ans
+        while queue:
+            num = queue.popleft()
+            if low<= num <=high:
+                res.append(num)
+            if num>high:
+                continue
+            last_digit = num%10
+            if last_digit<9:
+                next_num = (num*10) + last_digit + 1
+                queue.append(next_num)
+        return res
